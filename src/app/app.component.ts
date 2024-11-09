82a26d56e0110aa17e61cce4936d96819a2010b9
Felipe Figueiredo <fjnfigueiredo@gmail.com>
2024-11-09 19:38:45 -0300

    feat: form-logic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,34 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+import { EmployeeModel } from './model/Employee';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ReactiveFormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent {}
+export class AppComponent {
+  employeeForm!: FormGroup;
+
+  employeeObj: EmployeeModel = new EmployeeModel();
+
+  constructor() {
+    this.createForm();
+  }
+
+  createForm() {
+    this.employeeForm = new FormGroup({
+      empId: new FormControl(this.employeeObj.empId),
+      name: new FormControl(this.employeeObj.name),
+      city: new FormControl(this.employeeObj.city),
+      state: new FormControl(this.employeeObj.state),
+      emailId: new FormControl(this.employeeObj.emailId),
+      contactNumber: new FormControl(this.employeeObj.contactNumber),
+      address: new FormControl(this.employeeObj.address),
+      pinCode: new FormControl(this.employeeObj.pinCode),
+    });
+  }
+}
